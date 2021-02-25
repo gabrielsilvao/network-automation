885434d0ad0761fbe86865fce388abe8060b3c97
Gabriel Silva - Notebook <loktwo13@gmail.com>
2021-02-25 15:28:30 -0300

    correcao jogo_adivinhacao

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -7,3 +7,6 @@ picked_number = random.randint(1, 10)
 while guess != picked_number:
     guess = int(input('Guess what number I just picked? '))
 print('Congrats, you just guessed my number!')
+
+
+# teste
