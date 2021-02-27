355a8b16abd031c0bf4b056f19a022788591584c
Gabriel Silva - Notebook <loktwo13@gmail.com>
2021-02-27 18:10:19 -0300

    netmiko com lista de ip

---
+++ python/netmiko_ip_address list.py
@@ -0,0 +1,27 @@
+from netmiko import ConnectHandler
+
+with open('devices.txt') as ip_list:
+    for ip in ip_list:
+        devices = {
+            "device_type": "cisco_ios",
+            "ip": ip,
+            "username": "cisco",
+            "password": "cisco",
+            "secret": "cisco",
+        }
+        print('-'*40, "\nConnecting to the device ", ip, '\n')
+
+
+        net_connect = ConnectHandler(devices)
+        net_connect.enable()
+
+        output = net_connect.send_command("show ip int br")
+        print(output)
+
+        for ip in range (10,51,10):
+            config_commands = ["interface loopback" + str(ip), "ip address 10.10." + str(ip) + ".1 255.255.255.255"]
+            output = net_connect.send_config_set(config_commands)
+            print(output)
+
+        output = net_connect.send_command("show ip int br")
+        print(output)
